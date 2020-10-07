87bc880a72bfc4561eabbe0726d174469fd840ee
Beksultan <beksultankaiypov@gmail.com>
2020-10-07 16:48:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
-$status 
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 
 
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   phrase = "It's-a me, Mario!"
   puts phrase
 end
